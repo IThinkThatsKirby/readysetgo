63aadf59b869d80f787ead79ce03ad15dd8e840c
kirby <hkellingson@gmail.com>
2022-10-13 14:13:18 -0500

    routes todone

--- goServer/go.mod
+++ goServer/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/gofiber/fiber/v2 v2.38.1 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- goServer/server.go
+++ goServer/server.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/goccy/go-json"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	// Start your fiber server
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Howdy, World!")
+	})
+	user := app.Group("/:user")
+
+	user.Get("/todo/", func(c *fiber.Ctx) error {
+		return c.SendString("Your todos for " + c.Params("user"))
+	})
+	user.Get("/todo/:id", func(c *fiber.Ctx) error {
+		return c.SendString("a specifc task with data")
+	})
+	user.Get("/todone/", func(c *fiber.Ctx) error {
+		return c.SendString("Your archived and completed tasks")
+	})
+	user.Get("/todone/:id", func(c *fiber.Ctx) error {
+		return c.SendString("Data on the completed task")
 	})
 	app.Listen(":8080")
 }
